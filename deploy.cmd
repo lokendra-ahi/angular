d84996b0275f6feef51a83c387ee978d21187a44
Lokendra <lokendraahirwar21@gmail.com>
2019-10-10 02:52:42 +0530

    updated deploy cmd

--- deploy.cmd
+++ deploy.cmd
@@ -1,24 +1,4 @@
-@if "%SCM_TRACE_LEVEL%" NEQ "4" @echo off
-
-:: ----------------------
-:: KUDU Deployment Script
-:: Version: 1.0.17
-:: ----------------------
-
-:: Prerequisites
-:: -------------
-
-:: Verify node.js installed
-where node 2>nul >nul
-IF %ERRORLEVEL% NEQ 0 (
-  echo Missing node.js executable, please install node.js, if already installed make sure it can be reached from current environment.
-  goto error
-)
-
-:: Setup
-:: -----
-
-setlocal enabledelayedexpansion
+:: Setupsetlocal enabledelayedexpansion
 
 SET ARTIFACTS=%~dp0%..\artifacts
 
